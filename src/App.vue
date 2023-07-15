eba73337e1cb8b05ec5c9242a7c423419e4717f9
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2023-07-14 17:58:33 -0700

    modify(calculator): Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="outercontainer">
+        <div class="innercontainer">
+            <InputBox/>
+            <CalculatorButtons/>
+            <Author/>
+        </div>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+    import { Options, Vue } from 'vue-class-component';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import CalculatorButtons from './components/CalculatorButtons.vue';
+    import InputBox from './components/InputBox.vue';
+    import Author from './components/Author.vue';
+
+    @Options({ components: { InputBox, CalculatorButtons, Author } })
+
+    export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+    @import 'styles/App.less';
 </style>
---
+++ src/components/Author.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="author_container">
+        <span class="author">Created by Alex</span>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue } from 'vue-class-component';
+
+    export default class Author extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit LESS to this component only -->
+<style scoped lang="less">
+    @import '../styles/Author.less';
+</style>
